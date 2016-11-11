d8265ec96f960a109baacf4922376fc473a37bdd
Otávio Vicente Ribeiro <otavio.r.filho@gmail.com>
2016-11-11 20:31:16 -0300

    Fixing bugs

--- desenvolvimento/dimension_length.m
+++ desenvolvimento/dimension_length.m
@@ -1,15 +1,33 @@
-function count = dimension_length(Arr)
+function len = dimension_length(Arr)
 %   This function cumputes the length of the given cluster dimension. It
 %   stops when a zero line is found.
 %   from num. This is usefull for counting non-zero elements
 
     count = 0;
+    len = [];
     
     dims = size(Arr);
-    for c=1:dims(1)
-        if(Arr(c,1) == 0 && Arr(c,2) == 0)
-            break;
+    
+%     for c=1:dims(1)
+%         if(Arr(c,1) == 0 && Arr(c,2) == 0)
+%             break;
+%         end
+%         count = count + 1;
+%     end
+    if length(dims) < 3
+        d = 1;
+    else
+        d = dims(3);
+    end
+    
+    for i = 1:d
+        for j = 1:dims(1)
+            if(Arr(j,1,i) == 0 && Arr(j,2,i) == 0)
+                break;
+            end
+            count = count + 1;
         end
-        count = count + 1;
+        len(i) = count;
+        count = 0;
     end
 end
--- desenvolvimento/segmentation.m
+++ desenvolvimento/segmentation.m
@@ -4,15 +4,18 @@ function [S C H] = segmentation(P, d)
 
 %   Variable initialization part
     i = 1;
-    j = 2;
-    k = 1;
+%   The sufix l and d represent, respectively, line and dimension of the
+%   clusters
+    jl = 1;
+    jd = 2;
     
-%  ml e md represent, respectively, line and dimension of the auxiliary
-%  cluster H
+    kl = 1;
+    kd = 1;
+    
     ml = 1;
     md = 1;
     
-    C(1,1:2) = P(1,1:2);
+    C(1,1:2,1) = P(1,1:2);
     H = [];
     S = [];
     
@@ -22,10 +25,11 @@ function [S C H] = segmentation(P, d)
             i = i+1;
             ml = ml+1;
         elseif pdist([P(i+2,1:2);P(i,1:2)]) < d
-            H(ml,1:2,md) = P(i+2, 1:2);
-            C(j,1:2) = P(i,1:2);
+            H(ml,1:2,md) = P(i+2,1:2);
+            C(jl,1:2,jd) = P(i+1,1:2);
             ml = ml+1;
-            j = j+1;
+            jd = j+1;
+            jd = 1;
             i = i+1;
         elseif pdist([P(i+2,1:2);P(i+1,1:2)]) < d       
             md = md+1;
@@ -34,13 +38,42 @@ function [S C H] = segmentation(P, d)
             ml = ml+2;
             i = i+2;
         else
-            C(j,1:2) = P(i+1,1:2);
-            j=j+1;
+            C(jl,1:2,jd) = P(i+1,1:2);
+            jd=jd+1;
+            jl = 1;
             md = md+1;
             ml = 1;
             H(ml,1:2,md) = P(i+2,1:2);
             ml = ml+1;
             i = i+2;
+        end
+    end
+        
+    lenH = dimension_length(H);
+
+    dimsH = size(H);
+    dimsC = size(C);
+
+    if(md > 1) 
+        md = dimsH(3);
+    end
+
+    if(jd > 2)
+        jd = dimsC(3) + 1;
+    else
+        jd = 2;
+    end
+    
+    while md > 0
+        lenC = dimension_length(C);
+        if lenH(md) <= max(lenC)
+            C(1:lenH(md),1:2,jd) = H(1:lenH(md),1:2,md);
+            jd = jd+1;
+            md = md-1;
+        else
+            S(1:lenH(md),1:2,kd) = H(1:lenH(md),1:2, md);
+            kd = kd+1;
+            md = md-1;
         end
     end
 end
